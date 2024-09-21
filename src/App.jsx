9dbd4f2224b2d64907cc45e12da25ce46d49b522
Lungastafa2 <lungastafa@gmail.com>
2024-09-21 22:38:13 +0200

    first commit with everything

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>lunga</h1>
+
+    <ul>
+      <li>Lunga</li>
+      <li>Stafa</li>
+      <li>Gaba</li>
+    </ul>
+    </>
+  )
+}
+
+export default App
